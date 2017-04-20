e29e757713e4195da23f9544cbeda4431c60381c
LucienJan <lain_000@163.com>
2017-04-20 21:16:49 +0800

    第二次上传

--- js/controller/controller.js
+++ js/controller/controller.js
@@ -3,7 +3,7 @@
 (function() {
 	var controllers = angular.module("newsApp.Controller", [])
 	controllers.controller("indexCtrl", ["$scope", function($scope) {
-		$scope.auther = "This page is written by LucienJan";
+		$scope.auther = "This project is written by LucienJan";
 		console.log("ok");
 		// 默认的选项
 		$scope.item = 0;
@@ -12,34 +12,89 @@
 		}
 	}])
 
-		// 推荐
-		.controller("recommendCtrl", ["$scope", function($scope) {
-			$scope.title = "推荐页面";
-			
-		}])
-
-		// 娱乐
-		.controller("entertainCtrl", ["$scope", function($scope) {
-			$scope.title = "娱乐页面";
-		}])
-
-		// 社会
-		.controller("societyCtrl", ["$scope", function($scope) {
-			$scope.title = "社会页面";
-		}])
-
-		// 军事
-		.controller("militaryCtrl", ["$scope", function($scope) {
-			$scope.title = "军事页面";
-		}])
-
-		// 登录页面
-		.controller("loginCtrl",["$scope",function($scope){
-			$scope.title = "登录页面";
-		}])
+	// 推荐
+	controllers.controller("recommendCtrl", ["$scope","$http","$timeout", function($scope,$http,$timeout) {
+		$scope.title = "推荐页面";
+		$scope.page = 0;
+		$scope.news = [];
+		$scope.isLoading = false;
+		$scope.loadMore = function(){
+			// 点击更多时出现loading
+			$scope.isLoading = true;
+			$timeout(function(){
+				$http({
+					url:"js/news.json",
+					method:"GET",
+					params:{
+						channle_id:6,
+						page:$scope.page++
+					}
+				}).then(function(data){
+					$scope.isLoading = false;
+					$scope.news = $scope.news.concat(data.data.news_list);
+				})
+			},2000)
+		}
+		$scope.loadMore();
 		
-		// 新闻详情页
-		.controller("detailCtrl", ["$scope","$http","$location",function($scope,$http,$location) {
-			$scope.title = "新闻详情页"
-		}])
+		$scope.searchName = "";
+		// 搜索框
+		$scope.isShowSearchBar = false;
+		$scope.showSearchBar = function(){
+			$scope.isShowSearchBar = true;
+		}
+		
+		// 清空搜索框
+		$scope.searchClear = function(){
+			$scope.searchName = "";
+		}
+		$scope.resetSearch = function(){
+			$scope.searchName = "";
+			$scope.isShowSearchBar = false;
+		}
+		
+		// 弹窗
+		$scope.isShowPop = false;
+		$scope.showPop = function(){
+			$scope.isShowPop = true;
+		}
+	}])
+
+	// 娱乐
+	controllers.controller("entertainCtrl", ["$scope", function($scope) {
+		$scope.title = "娱乐页面";
+	}])
+
+	// 社会
+	controllers.controller("societyCtrl", ["$scope", function($scope) {
+		$scope.title = "社会页面";
+	}])
+
+	// 军事
+	controllers.controller("militaryCtrl", ["$scope", function($scope) {
+		$scope.title = "军事页面";
+	}])
+
+	// 登录页面
+	controllers.controller("loginCtrl",["$scope",function($scope){
+		$scope.title = "登录页面";
+	}])
+	
+	// 新闻详情页
+	controllers.controller("detailCtrl", ["$scope","$http","$location","$state",function($scope,$http,$location,$state) {
+		$scope.title = "新闻详情页";
+		console.log($location);
+		console.log($location.search);
+		console.log($state.params);
+		$http:({
+			url:"js/detail.json",
+			method:"GET",
+			params:{
+				id:$state.params.id
+			}
+		}).then(function(data){
+			console.log(data);
+			$scope.newdata = data.data.news_list[0];
+		})
+	}])
 })()
--- js/directive/directive.js
+++ js/directive/directive.js
@@ -17,16 +17,43 @@
 	//轮播图组件
 	directives.directive("myswiper", function() {
 		return {
-			templateUrl:"directives/myswiper.html",
-			link:function(scope,ele,attr){
-				
+			templateUrl: "directives/myswiper.html",
+			link: function(scope, ele, attr) {
+				var swiper = new Swiper('.swiper-container', {
+					pagination: '.swiper-pagination',
+					nextButton: '.swiper-button-next',
+					prevButton: '.swiper-button-prev',
+					paginationClickable: true,
+					spaceBetween: 30,
+					centeredSlides: true,
+					autoplay: 2500,
+					autoplayDisableOnInteraction: false
+				});
 			}
 		}
 	})
 	// 新闻列表组件
-	directives.directive("mylist",function(){
+	directives.directive("mylist", function() {
 		return {
-			templateUrl:"directives/mylist.html"
+			templateUrl: "directives/mylist.html"
+		}
+	})
+	// 搜索框组件
+	directives.directive("mysearch", function(){
+		return {
+			templateUrl:"directives/mysearch.html"
+		}
+	})
+	// loading遮罩
+	directives.directive("myloading",function(){
+		return {
+			templateUrl:"directives/myloading.html"
+		}
+	})
+	// 弹窗(点击header)
+	directives.directive("mypop",function(){
+		return {
+			templateUrl:"directives/mypop.html"
 		}
 	})
 })();
